a0bb41f943e74a14e02a07de58b2f82c5e7ec266
BCEwaynes <96417650+BCEwaynes@users.noreply.github.com>
2022-01-03 11:55:39 +1100

    cc

--- greetings.go
+++ greetings.go
@@ -1,11 +1,12 @@
-package main
+package greetings
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
 )
 
-func main() {
-	fmt.Println(quote.Go())
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
